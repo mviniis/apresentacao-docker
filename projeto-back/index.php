17014693f5d8184fae9eea1b9c283986fc3a8e0b
Matheus Vinicius <53922640+mviniis@users.noreply.github.com>
2024-10-13 01:09:49 +0000

    correcao do CORS

--- projeto-back/index.php
+++ projeto-back/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 $ipContainer = shell_exec('hostname -I');
 
 header('Content-Type: application/json');
